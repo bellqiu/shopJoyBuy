3213ece21d7bb371662fefdf6550a93f7b4ea2ba
szhu.spark@gmail.com <szhu.spark@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2012-12-20 14:27:53 +0000

    Home filter
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/shopJoyBuy@1239 c8aa77e7-4d14-485d-9416-cb5e655923dd

---
+++ src/com/spshop/web/HomeFilter.java
@@ -0,0 +1,62 @@
+/*
+ * Project: iSAPort
+ * Copyright (c) 2012 HP. All Rights Reserved.
+ */
+package com.spshop.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * @author <link href="wan-shan.zhu@hp.com">Spark Zhu</link>
+ * @version 1.0
+ */
+public class HomeFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		try{
+		
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		
+		HttpServletRequest httpReq = (HttpServletRequest) request;
+		
+		String serverName = httpReq.getServerName();
+		
+		if(!serverName.matches("www\\.\\w+.*") 
+				&& !serverName.matches("\\d+\\.\\d+.*")
+				&& !serverName.matches("localhost.*")){
+			HttpServletResponse httpResp = (HttpServletResponse) response;
+			httpResp.sendRedirect(request.getScheme()+":www."+request.getServerName());
+			return;
+		}
+		
+		}finally{
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
