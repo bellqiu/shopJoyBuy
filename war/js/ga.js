fbca62face700856913184115997de7022494d1a
realliam@gmail.com <realliam@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2013-01-30 09:47:48 +0000

    ga
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/shopJoyBuy@1436 c8aa77e7-4d14-485d-9416-cb5e655923dd

--- war/js/ga.js
+++ war/js/ga.js
@@ -1,11 +1,8 @@
 var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-27926805-1']);
-  _gaq.push(['_setCampSourceKey', 'utm_source']);
-  _gaq.push(['_setCampMediumKey', 'utm_medium']);
-  _gaq.push(['_setCampContentKey', 'utm_content']);
-  _gaq.push(['_setCampTermKey', 'utm_keyword']);
-  _gaq.push(['_setCampNameKey', 'utm_campaign']);
+  _gaq.push(['_setAccount', 'UA-38082632-1']);
+  _gaq.push(['_setDomainName', 'joybuy.co.uk']);
   _gaq.push(['_trackPageview']);
+
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
